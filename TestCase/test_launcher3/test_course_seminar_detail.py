87fb6098cb0da056a830f108e530c622147d6c14
zheng.zhang <zheng.zhang@xgimi.com>
2019-10-23 10:14:08 +0800

    [add]添加launcher3.0后10条接口用例

---
+++ TestCase/test_launcher3/test_course_seminar_detail.py
@@ -0,0 +1,48 @@
+import json
+import requests
+from Common.params import Params
+from Conf.Config import Config
+from Common import Assert
+
+
+class TestCourseDetail:
+    def test_response_code(self):
+        """
+        用例描述：课程表/单课件专题详情接口返回状态码
+        :return:
+        """
+        conf = Config()
+        params = Params()
+        test = Assert.Assertions()
+
+        host = conf.debug_gossapi_host
+        api_url = host + "/course/seminarDetail"
+        params.add_param("contentId", "4512154")
+        params = params.encrypt_data()
+        headers = conf.debug_headers
+
+        res = requests.post(api_url, params=params, headers=headers)
+        assert test.assert_code(res.status_code, 200)
+
+    def test_jsonschema_validate(self):
+        """
+        用例描述：课程表/单课件专题详情接口返回数据JsonSchema验证
+        :return:
+        """
+        conf = Config()
+        params = Params()
+        test = Assert.Assertions()
+
+        host = conf.debug_gossapi_host
+        api_url = host + "/course/seminarDetail"
+        params.add_param("contentId", "4512154")
+        params = params.encrypt_data()
+        headers = conf.debug_headers
+        schema = json.load(open(conf.json_schema_path + "/course_seminar_detail_schema.json"))
+
+        res = requests.post(api_url, params=params, headers=headers)
+        assert test.assert_jsonschema(res.json(), schema)
+
+
+if __name__ == '__main__':
+    pass
